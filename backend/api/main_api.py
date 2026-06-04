ae2241a77960f47ed496c326c54aaf8e06a9c0e0
Benny HOUNSOU <hounsoubenny@gmail.com>
2026-06-04 15:22:25 +0100

    Clean up main_api.py by removing docstring
    Removed unnecessary docstring and author comment.

--- backend/api/main_api.py
+++ backend/api/main_api.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jun  2 08:23:56 2026
-
-@author: hounsousamuel
-"""
 
 import os, sys
 sys.path.insert(1, os.path.dirname(os.path.abspath(os.path.join(__file__, "..", ".."))))
